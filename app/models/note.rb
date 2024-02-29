dcc4dfb42aae3deb60fb52da5b8ae208fab45676
Kirshin Aleksandr <aleksandr.s.kirshin@gmail.com>
2024-02-29 15:01:43 +0700

    20240229_1500

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,4 +1,6 @@
 # app/models/note.rb
 class Note < ApplicationRecord
   has_and_belongs_to_many :tags
+
+  validates_presence_of :title, :content
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,6 @@
 # app/models/tag.rb
 class Tag < ApplicationRecord
   has_and_belongs_to_many :notes
+
+  validates_presence_of :name
 end
